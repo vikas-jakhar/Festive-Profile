fb82cefa2474261ac94e092be7fedff4739fca0e
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-02-21 10:32:30 +0530

    first commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,6 @@
+const menuicon = document.querySelector(".menuicon");
+const menulist = document.querySelector(".menu");
+
+menuicon.addEventListener("click", function () {
+    menulist.classList.toggle("show")
+})
